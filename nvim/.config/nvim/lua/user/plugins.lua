ba1c7fe22f5bffd2be5adf6a45d98c4fd187ec71
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2022-10-19 12:41:23 +0100

    feat(nvim): add ansible plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -57,6 +57,7 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "jvirtanen/vim-hcl"
+  use "pearofducks/ansible-vim"
 
   -- Color schemes
   use "arcticicestudio/nord-vim"
