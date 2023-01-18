a78d23ed4272b323bbef6849b56945e3e6e88536
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2023-01-18 13:55:38 +0000

    refactor(nvim): rewrite nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,2 @@
-require "user.core.colorscheme"
-require "user.core.keymaps"
-require "user.core.options"
-require "user.plugins"
-require "user.plugins.cmp"
-require "user.plugins.nvim-tree"
+require('user')
 
---
+++ nvim/.config/nvim/lua/user/init.lua
@@ -0,0 +1,4 @@
+require('user.packer')
+require('user.set')
+require('user.remap')
+
