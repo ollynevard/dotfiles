e4eae1d9d164f9c20e0e4810d67f368f2c5cf270
Olly Nevard <150187+ollynevard@users.noreply.github.com>
2024-01-22 12:14:26 +0000

    wip

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+--
+-- Colour scheme - https://github.com/catppuccin/nvim
+--
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
+} 
+
